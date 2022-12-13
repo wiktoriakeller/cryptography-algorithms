243fe71d4119d3a9d86e0002c9cc3223885843c2
wiktoria <wiktoriakeller2109@gmail.com>
2022-12-13 17:22:19 +0100

    Update MQV algorithm

--- MQV/MQV.py
+++ MQV/MQV.py
@@ -1,15 +1,17 @@
 import random
-from math import sqrt
+from math import sqrt, floor
 
 def sieve_of_eratosthenes(n):
-    primes = [True for _ in range(n+1)]
-    for i in range(2, sqrt(n) + 1):
+    primes = [True for _ in range(n)]
+    upper_bound = floor(sqrt(n) + 1)
+
+    for i in range(2, upper_bound):
         if (primes[i] == True):
             for j in range(i * i, n, i):
                 primes[j] = False
  
     numbers = []
-    for i in range(2, n + 1):
+    for i in range(2, n):
         if primes[i] == True:
             numbers.append(i)
 
@@ -40,10 +42,20 @@ def find_smallest_primitive_root(n):
 
     return None
 
-min = 100
-max = 500
+# calculate shared secret using long term and short term keys
+# e.g. person Alice: (bob short term key x bob long term key^(bob short term key))
+#to the power (alice short term private key + alice long term private key x alice short term public key)
+def get_shared_secret(send_short_public, send_long_public, my_short_private, my_short_public, my_long_private, n):
+    power = send_short_public * pow(send_long_public, send_short_public)
+    exponent = my_short_private + (my_long_private * my_short_public)
+    shared_secret = pow(power, exponent, n)
+    return shared_secret
 
-n = 4999
+min_key = 100
+max_key = 500
+max_prime = 9999
+
+n = sieve_of_eratosthenes(max_prime)[-1]
 print(f"Prime number n: {n}")
 
 # primitive root modulo n
@@ -52,43 +64,39 @@ print(f"Primitive root modulo n: {g}")
 print(f"Check if primitive root modulo n is valid: {check_if_primitive_root(g, n)}")
 
 # Alice long term keys
-long_term_private_a = random.randint(min, max)
+long_term_private_a = random.randint(min_key, max_key)
 print(f"Alice, long term private key: {long_term_private_a} ")
 
 long_term_public_a = pow(g, long_term_private_a, n)
 print(f"Alice, long term public key: {long_term_public_a} sending to Bob")
 
 # Bob long term keys
-long_term_private_b = random.randint(min, max)
+long_term_private_b = random.randint(min_key, max_key)
 print(f"Bob long term private key: {long_term_private_b}")
 
 long_term_public_b = pow(g, long_term_private_b, n)
 print(f"Bob, long term public key: {long_term_public_b}, sending to Alice")
 
 # Alice short term keys
-short_term_private_a = random.randint(min, max)
+short_term_private_a = random.randint(min_key, max_key)
 print(f"Alice, short term private key: {short_term_private_a} ")
 
 short_term_public_a = pow(g, short_term_private_a, n)
 print(f"Alice, short term public key: {short_term_public_a} sending to Bob")
 
 # Bob long term keys
-short_term_private_b = random.randint(min, max)
+short_term_private_b = random.randint(min_key, max_key)
 print(f"Bob short term private key: {short_term_private_b}")
 
 short_term_public_b = pow(g, short_term_private_b, n)
 print(f"Bob, short term public key: {short_term_public_b}, sending to Alice")
 
 # Alice shared secret 
-power_a = short_term_public_b * pow(long_term_public_b, short_term_public_b)
-exponent_a = short_term_private_a + (long_term_private_a * short_term_public_a)
-shared_secret_a = pow(power_a, exponent_a, n)
+shared_secret_a = get_shared_secret(short_term_public_b, long_term_public_b, short_term_private_a, short_term_public_a, long_term_private_a, n)
 print(f"Alice, shared secret: {shared_secret_a}")
 
 # Bob shared secret
-power_b = short_term_public_a * pow(long_term_public_a, short_term_public_a)
-exponent_b = short_term_private_b + (long_term_private_b * short_term_public_b)
-sahred_secret_b = pow(power_b, exponent_b, n)
-print(f"Bob, shared secret: {sahred_secret_b}")
+shared_secret_b = get_shared_secret(short_term_public_a, long_term_public_a, short_term_private_b, short_term_public_b, long_term_private_b, n)
+print(f"Bob, shared secret: {shared_secret_b}")
 
-assert shared_secret_a == sahred_secret_b
+assert shared_secret_a == shared_secret_b
